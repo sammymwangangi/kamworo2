0166da38a7f8772405fff2e07005373c329d6678
sammymwangangi <sammymwinzi2@gmail.com>
2018-06-16 10:12:46 +0300

    first commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+use App\Product;
+
+use DB;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+            'image' => 'image|nullable|max:2048'
+        ]);
+
+        if($request->hasFile('image')){
+            $filenameWithExt=$request->file('image')->getClientOriginalName();
+            $filename=pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            $extension=$request->file('image')->getClientOriginalExtension();
+            $fileNameToStore=$filename.'_'.time().'.'.$extension;
+            $path=$request->file('image')->storeAs('public/products',$fileNameToStore);
+        } else{
+            $fileNameToStore='noimage.jpg';
+        }
+
+        $product = new Product;
+        $product->name=$request->input('name');
+        $product->description=$request->input('description');
+        $product->price=$request->input('price');
+        $product->category_id=$request->input('category_id');
+        $product->image=$fileNameToStore;
+        $product->save();
+
+        return redirect('/products')->with('success', 'Product Created!!!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product=Product::find($id);
+        return view('products.show')->with('products', $post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product=Product::find($id);
+        return view('products.edit')->with('product',$product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'category_id' => 'required',
+            'image' => 'image|nullable|max:2048'
+        ]);
+
+        if($request->hasFile('image')){
+            $filenameWithExt=$request->file('image')->getClientOriginalName();
+            $filename=pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            $extension=$request->file('image')->getClientOriginalExtension();
+            $fileNameToStore=$filename.'_'.time().'.'.$extension;
+            $path=$request->file('image')->storeAs('public/products',$fileNameToStore);
+        } else{
+            $fileNameToStore='noimage.jpg';
+        }
+
+        $product = Product:find($id);
+        $product->name=$request->input('name');
+        $product->description=$request->input('description');
+        $product->price=$request->input('price');
+        $product->category_id=$request->input('category_id');
+        if($request->hasFile('image')){
+            $product->image=$fileNameToStore;
+        }
+        $product->image=$fileNameToStore;
+        $product->save();
+
+        return redirect('/products')->with('success', 'Product Updated!!!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product=Product::find($id);
+
+        if($product->image != 'noimage.jpg'){
+            Storage::delete('public/products/'.$product->image);
+        }
+
+        $product->delete();
+        return redirect('/products')->with('success', 'Product Removed!!!');
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.app')
+
+@section('content')
+	<div class="container justify-content-center">
+		<div class="card">
+			<form>
+				<div class="form-group">
+				    <label for="exampleFormControlInput1">Email address</label>
+				    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+				</div>
+				<div class="form-group">
+				    <label for="exampleFormControlSelect1">Example select</label>
+				    <select class="form-control" id="exampleFormControlSelect1">
+				      <option>1</option>
+				      <option>2</option>
+				      <option>3</option>
+				      <option>4</option>
+				      <option>5</option>
+				    </select>
+				</div>
+				<div class="form-group">
+				    <label for="exampleFormControlSelect2">Example multiple select</label>
+				    <select multiple class="form-control" id="exampleFormControlSelect2">
+				      <option>1</option>
+				      <option>2</option>
+				      <option>3</option>
+				      <option>4</option>
+				      <option>5</option>
+				    </select>
+				</div>
+				<div class="form-group">
+				    <label for="exampleFormControlTextarea1">Example textarea</label>
+				    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+				</div>
+			    <div class="form-group">
+				    <label for="image">Example file input</label>
+				    <input type="file" class="image" id="image">
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
